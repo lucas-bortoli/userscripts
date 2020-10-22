baa7e1d28c495ae765bebd6de198fd6a80e6cece
pantheonlbs <51049108+pantheonlbs@users.noreply.github.com>
2020-10-22 09:26:26 -0300

    Listen only

--- scripts/chamada.user.js
+++ scripts/chamada.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chamada
 // @namespace    https://github.com/pantheonlbs/
-// @version      1.1
+// @version      1.4
 // @description  Responder chamada
 // @author       pantheonlbs
 // @match        https://*/html5client/*
@@ -9,43 +9,48 @@
 // ==/UserScript==
 
 (function() {
-  	function createCheckbox() {
-    	let checkbox = document.createElement('input')
-      checkbox.setAttribute('type', 'checkbox')
-      checkbox.id = '__script_on'
-      checkbox.style = 'position: fixed;top: 50%;right: 2em;z-index: 999;'
-      checkbox.title = '[script] Auto-chamada ligada?'
-      document.body.appendChild(checkbox)
-      return checkbox
-    }
+	function createCheckbox() {
+		let checkbox = document.createElement('input')
+		checkbox.setAttribute('type', 'checkbox')
+		checkbox.id = '__script_on'
+		checkbox.style = 'position: fixed;top: 50%;right: 2em;z-index: 999;'
+		checkbox.title = '[script] Auto-chamada ligada?'
+		checkbox.checked = true
+		document.body.appendChild(checkbox)
+		return checkbox
+	}
 
-    function popup(titulo, texto) {
-        let html = `
+	function popup(titulo, texto) {
+		let html = `
             <div id="__popup" style="z-index: 999; display: block; position: float">
                 <h2 style="margin-top: 0">${titulo}</h2>
                 <p>${texto}</p>
                 <button onclick="document.querySelector('#__wrapper').remove()">Fechar popup</button>
             </div>`
 
-       let wrapper = document.createElement('div')
+		let wrapper = document.createElement('div')
 
-       wrapper.id = '__wrapper'
-       wrapper.style = `display:flex;position:fixed;z-index:999;top:50%;left:50%;transform:translate(-50%,-50%);width:300px;border:1px solid gray;padding:2em;background:white;`
-       wrapper.innerHTML = html
-       document.body.appendChild(wrapper)
-    }
-  
-    let TURMA = 'A'
-		let checkbox_on = createCheckbox()
+		wrapper.id = '__wrapper'
+		wrapper.style = `display:flex;position:fixed;z-index:999;top:50%;left:50%;transform:translate(-50%,-50%);width:300px;border:1px solid gray;padding:2em;background:white;`
+		wrapper.innerHTML = html
+		document.body.appendChild(wrapper)
+	}
+	
+	let checkbox_on = createCheckbox()
 
-		let i = setInterval(_ => {
-				let botao = document.querySelector(`button[aria-label="${TURMA}"]`)
+	let i = setInterval(_ => {
+		let botao_audio = document.querySelector(`button[aria-label="Listen only"]`)
+		let botao_enquete = document.querySelector(`button[aria-label="A"]`) || document.querySelector(`button[aria-label="Yes"]`) || document.querySelector(`button[aria-label="True"]`)
 
-				if (botao && checkbox_on.checked) {
-						let horario = new Date().toLocaleTimeString()
+		if (botao_enquete && checkbox_on.checked) {
+			let horario = new Date().toLocaleTimeString()
 
-						botao.click()
-						popup('Chamada', `Respondi a chamada!<br>Horário: <b>${horario}</b>`)
-				}
-		}, 2 * 1000)
+			botao_enquete.click()
+			popup('Chamada', `Respondi a chamada!<br>Horário: <b>${horario}</b>`)
+		}
+
+		if (botao_audio) {
+			botao_audio.click()
+		}
+	}, 2 * 1000)
 })()
